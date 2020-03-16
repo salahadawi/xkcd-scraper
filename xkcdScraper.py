40d89d97aeaa7f9ad33907064d9062fbd8799167
sadawi <sadawi.hahli144@hotmail.com>
2020-03-16 17:22:27 +0200

    change output, next image now comes from button

--- xkcdScraper.py
+++ xkcdScraper.py
@@ -7,24 +7,26 @@ try:
 	os.mkdir("xkcd")
 except:
 	pass
+
+res = requests.get("http://xkcd.com/1")
+res.raise_for_status()
 while(1):
-	if i == 404:
-		i+= 1
-	try:
-		res = requests.get("http://xkcd.com/" + str(i))
-		res.raise_for_status()
-	except:
-		print("Download finished.\n" + str(i) + " images downloaded.\n")
-		sys.exit()
 	soup = bs4.BeautifulSoup(res.text, "html.parser")
 	try:
 		imageLink = "http:" + soup.select_one("#comic > img[src]")["src"]
 	except:
 		imageLink = "http:" + soup.select_one("#comic > a > img[src]")["src"]
 	imageResponse = requests.get(imageLink)
-	file = open("xkcd/" + str(i) + re.search("\.\w+$", imageLink)[0], "wb")
+	file = open("xkcd/" + str(i)+ "-" + re.search("[^\/]+$", imageLink)[0], "wb")
 	for chunk in imageResponse.iter_content(1024):
 		file.write(chunk)
 	file.close()
-	print("Image " + re.search("[^\/]+$", imageLink)[0] + " downloaded.")
-	i += 1
+	print("Image number " + str(i) + " named " + re.search("[^\/]+$", imageLink)[0] + " downloaded.")
+	linkNext = "http://xkcd.com" + soup.select_one("#middleContainer > ul > li:nth-of-type(4) > a")["href"]
+	try:
+		res = requests.get(linkNext)
+		res.raise_for_status()
+	except:
+		print("Download finished.\n" + str(i) + " images downloaded.\n")
+		sys.exit()
+	i = int(soup.select_one("#middleContainer > ul > li:nth-of-type(4) > a")["href"].strip("/"))
